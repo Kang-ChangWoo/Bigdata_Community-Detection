0f2c68ecaa5f5749aa310928de4428ee75664143
changwoo <branden.c.w.kang@gmail.com>
2024-06-11 01:13:48 +0900

    update

--- N4-test.py
+++ N4-test.py
@@ -177,7 +177,7 @@ if __name__ == "__main__":
     #parser.add_argument('--test_path', type=str, default='/root/storage/implementation/Lecture-BDB_proj/Bigdata_Community-Detection/data/train/TC1-all_including-GT', help='path of test dataset') # path/to/yours
     parser.add_argument('--device', type=str, default='cuda', help='device')
     parser.add_argument('--model_path', type=str, default='./ckpt/correction-last.pt', help='path to the best model')
-    parser.add_argument('--output_file', type=str, default='inference_result.txt', help='path to save the output results')
+    parser.add_argument('--output_file', type=str, default='vis/inference_result.txt', help='path to save the output results')
     parser.add_argument('--testset', type=str, default='real', help='-')
     #parser.add_argument('--pth', type=str, default='basic', help='-')
 
